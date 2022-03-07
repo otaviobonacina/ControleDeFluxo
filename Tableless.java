35de8484d2788314bb7dd34cbdccdf0d3d680a1c
otaviobonacina <otavio.bernado@fatec.sp.gov.br>
2022-03-07 17:59:24 +0000

    Tablelesscor

--- Tableless.java
+++ Tableless.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
 public class Tableless {
     public static void main(string[] args){
-        int v1=1;
-        int v2=2;
-        System.out.println((v1 > v2 ? "1 é maior" : "2 é maior"));
+        double v1
+        double v2;
+        Scanner leitura=new Scanner(System.in);
+        System.out.println("digite um numero");
+       v1=leitura.nexDouble();
+       System.out.println("digite um numero");
+       v2=leitura.nexDouble();
+        System.out.println((v1 > v2 ?+v1 "1 é maior" :+v2 "2 é maior"));
         if (v1>v2){
             Sysrem.out.orintln("1 é maior");
         } else {
