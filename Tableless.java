f22561da533f4e71e2541d3dc25cb834d05bddc7
otaviobonacina <otavio.bernado@fatec.sp.gov.br>
2022-03-07 17:52:58 +0000

    Tablelessmmm

---
+++ Tableless.java
@@ -0,0 +1,13 @@
+public class Tableless {
+    public static void main(string[] args){
+        int v1=1;
+        int v2=2;
+        System.out.println((v1 > v2 ? "1 é maior" : "2 é maior"));
+        if (v1>v2){
+            Sysrem.out.orintln("1 é maior");
+        } else {
+            System.out.println("2 é maior");
+        }
+    }
+    
+}
